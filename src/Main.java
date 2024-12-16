e9771349473be8a8759242827d508ac1091fb6be
Saka Koutarou <e245707@ie.u-ryukyu.ac.jp>
2024-12-16 22:55:13 +0900

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 条件1: String型変数 str を null に設定
+        String str = null;
+
+        // 条件2: str.length() を呼び出してエラーを発生させる
+        System.out.println("Length of the string: " + str.length());
+    }
+}
